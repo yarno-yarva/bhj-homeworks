59d4882f87567724ba8b75660745c24a49d90cd9
yarno <yarno@me.com>
2020-09-22 18:02:40 +0200

    Fix task 1 & 2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,16 +1,35 @@
+
+
+
+let hours = 0;
+let minutes = 1;
+let seconds = 15;
+
+const timer = document.getElementById("timer");
+
+timer.textContent = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+
 function countdown() {
-     const output = document.getElementById("timer");
      let id = setInterval(count, 1000);
+     
      function count(){
-     	if (output.textContent == 0) {
+  	if (seconds == 0 && minutes == 0 && hours == 0)  {
 	 alert('Вы победили в конкурсе!');
 	 clearInterval(id);
      } else {
-     	output.textContent -=1;
-     }
-    
- }
-}
+     	seconds -=1;
+     	if (seconds == 0 && minutes > 0) {
+       	minutes -= 1;
+     	seconds = 59;
+      } if (minutes == 0 &&  hours > 0) {
+      	hours -= 1;
+      	minutes = 59;
+      } 
+      timer.textContent = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+   	 }
+    }
+  }
 
 countdown();
+
 
