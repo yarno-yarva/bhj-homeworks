6ac37bf92db66b0fe7e10c7f5310970455b7a069
yarno <yarno@me.com>
2020-09-27 13:00:30 +0200

    Fix task menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,17 +3,31 @@ let links = Array.from(menuLink);
 
 for (let i = 0; i <= links.length - 1 ; i++) {
   let link = links[i];
-   link.onclick = function() {
-  	clear();
-  	let selector = link.closest('li').querySelector('ul');
-        if (selector != null) {
-     		selector.classList.add('menu_active');
-    		return false;
- 	   	} 
+
+  link.onclick = function() {
+
+    let selector = link.closest('li').querySelector('ul');
+    if (selector != null) {
+      if (selector.classList.contains('menu_active')) {
+          document.getElementsByClassName('menu_active').item(0).classList.toggle('menu_active');
+          return false;
+      } else {
+          clear()
+          selector.classList.add('menu_active')
+        }
+         return false;
+      } 
     }
 }
+
 
 function clear() {
 	if (document.getElementsByClassName('menu_active').length == 1)
     document.getElementsByClassName('menu_active').item(0).classList.toggle('menu_active');
 }
+
+
+
+
+
+ //&& classList.contains('menu_active') == false 
