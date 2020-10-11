fc00d523685264e08a3fe6a0db493d74608e0f14
yarno <yarno@me.com>
2020-10-11 16:25:42 +0200

    Fix task

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -9,10 +9,8 @@ for (let i = 0; i < divs.length; i++ ) {
 	let div = divs[i];
 
 	if (div.getBoundingClientRect().top > 0 && div.getBoundingClientRect().top < window.innerHeight) {
-		if (div.classList.contains('reveal_active') != true ) {
 		div.classList.add('reveal_active');	
-		}
-	} else if (div.classList.contains('reveal_active')) {
+	} else {
 		div.classList.remove('reveal_active');
 	    }
     }
